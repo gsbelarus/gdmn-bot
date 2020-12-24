c430e2f341328c4b68afab1c0c8afbfd4385620a
AndreiK <gs1994@gmail.com>
2020-12-24 18:07:48 +0300

    access to nbrb web site was changed changed to https urls

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,8 @@ import { IHourType } from "./types";
 import { ILocString, stringResources } from "./stringResources";
 
 export const MINDATE = new Date(2018, 0, 1);
-export const URLNBRBRATES = "http://www.nbrb.by/API/ExRates/Rates";
-export const URLNBRBCURRENCIES = "http://www.nbrb.by/API/ExRates/Currencies";
+export const URLNBRBRATES = "https://www.nbrb.by/API/ExRates/Rates";
+export const URLNBRBCURRENCIES = "https://www.nbrb.by/API/ExRates/Currencies";
 
 export const hourTypes: ILocString[] = [
   stringResources.appearanceShift,
