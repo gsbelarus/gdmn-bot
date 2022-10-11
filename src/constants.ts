8bbc08d97cb467638d510d94d9fbdc650c449859
andreik <gs1994@gmail.com>
2022-10-11 15:54:47 +0200

    access nbrb via bouncer

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,10 @@
 import { ILocString, stringResources } from "./stringResources";
 
 export const MINDATE = new Date(2018, 0, 1);
-export const URLNBRBRATES = "https://www.nbrb.by/api/exrates/rates";
-export const URLNBRBCURRENCIES = "https://www.nbrb.by/api/exrates/currencies";
+export const URLNBRBRATES =      "http://213.184.249.125:8099/api/exrates/rates";
+export const URLNBRBCURRENCIES = "http://213.184.249.125:8099/api/exrates/currencies";
+//export const URLNBRBRATES = "https://www.nbrb.by/api/exrates/rates";
+//export const URLNBRBCURRENCIES = "https://www.nbrb.by/api/exrates/currencies";
 
 export const hourTypes: ILocString[] = [
   stringResources.appearanceShift,
